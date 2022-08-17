1dd3e0cdbe181c52521d4e0452500d1c05e701d0
Marshall Bowers <elliott.codes@gmail.com>
2022-08-16 21:02:38 -0400

    Wire up the rule

--- src/pr-hygiene.ts
+++ src/pr-hygiene.ts
@@ -1,3 +1,11 @@
+import { DangerDSLType } from 'danger';
+import { useImperativeMood } from './rules';
+
+declare var danger: DangerDSLType;
+declare function warn(message: string): void;
+
 export interface PrHygieneOptions {}
 
-export const prHygiene = ({}: PrHygieneOptions) => {};
+export const prHygiene = ({}: PrHygieneOptions) => {
+  useImperativeMood({ warn })(danger.github.pr.title);
+};
